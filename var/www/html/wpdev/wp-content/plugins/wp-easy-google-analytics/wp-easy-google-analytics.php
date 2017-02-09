35096b5d92376cc2d5f9bb6394124903819edd49
BDPweb <>
2017-02-09 22:44:55 +0100

    WP Easy Google Analytics

--- var/www/html/wpdev/wp-content/plugins/wp-easy-google-analytics/wp-easy-google-analytics.php
+++ var/www/html/wpdev/wp-content/plugins/wp-easy-google-analytics/wp-easy-google-analytics.php
@@ -98,22 +98,27 @@ function process_wp_easy_google_analytics_options() {
     exit;
 }
 
-add_action('wp_head', 'gnu_terry_pratchett_meta');
-?>
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+add_action('wp_head', 'wp_easy_google_analytics_script');
 
-    ga('create', '', 'auto');
-    ga('send', 'pageview');
+function wp_easy_google_analytics_script() {
+    $options = get_option('wp_easy_analytics_options');
+    ?>
+    <script type="text/javascript">
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-</script>
+        ga('create', '<?php echo esc_html($options['ga_account_name']); ?>', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+    <?php
+}
